d9ee718a5c58ee67138e9f90bf7c94a782d08087
Edgar Patricio Sánchez Malla <epsm96@gmail.com>
2021-07-26 22:10:43 -0500

    Desarrollo del frontend, incluir vista principal en la linea gráfica de la UNL

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,22 @@ import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
+import loading from './img/loading.svg';
+import valid from './img/valid.svg';
+import invalid from './img/invalid.svg';
+
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
+      // Obtener el proveedor de red y la instancia web3.
       const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
+      // Usar web3 para obtener las cuentas de los usuarios.
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
+      // Obtener la instancia del contrato.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
@@ -23,13 +27,13 @@ class App extends Component {
         deployedNetwork && deployedNetwork.address,
       );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
+      // Establece el estado de web3, cuentas y contrato, y luego procede
+      // con un ejemplo de interacción con los métodos del contrato.
       this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
-      // Catch any errors for any of the above operations.
+      // Captura de errores para cualquiera de las operaciones anteriores.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `No se ha podido cargar la web3, las cuentas o el contrato. Comprueba la consola para ver los detalles.`,
       );
       console.error(error);
     }
@@ -38,25 +42,33 @@ class App extends Component {
   runExample = async () => {
     const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
+    // Almacena un valor determinado, 5 por defecto.
     await contract.methods.set(5).send({ from: accounts[0] });
 
-    // Get the value from the contract to prove it worked.
+    // Obtener el valor del contrato para demostrar que ha funcionado.
     const response = await contract.methods.get().call();
 
-    // Update state with the result.
+    // Actualizar el estado con el resultado.
     this.setState({ storageValue: response });
   };
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return (
+        <div className="row ">
+          <div className="col s12">
+            <div className="card">
+              <div className="card-content">
+                <img className='img-result' src={loading} /> <br />
+                <span className="second-title-cost">Cargando Web3, cuentas, y contrato...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
         <p>
           If your contracts compiled and migrated successfully, below will show
           a stored value of 5 (by default).
