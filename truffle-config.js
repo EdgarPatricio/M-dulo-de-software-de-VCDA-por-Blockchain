447c5d46b405ecc8d56e95e6e59e52e00984bb3e
Edgar Patricio Sánchez Malla <edgar.sanchez@unl.edu.ec>
2021-08-21 17:38:14 -0500

    Configuración para probar la solución en la tesnet de Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "hover hospital firm humor ride fragile spoon deposit imitate riot action add";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +8,12 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    rinkeby:{
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/07d2ffb1ab384200959b4bc9b748e521");
+      },
+      network_id: '4',
     }
   },
   compilers: {
